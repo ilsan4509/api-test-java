072cc43ea23d6cabf2c78bd450ef24692b8a9ad1
Seonggon <ilsan4509@naver.com>
2025-03-30 19:45:47 +0100

    Add parameterized tests using TestNG DataProvider and static JSON payload handling

--- src/Basics.java
+++ src/Basics.java
@@ -4,6 +4,10 @@ import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.testng.Assert;
 
 import files.ReUsableMethods;
@@ -12,20 +16,22 @@ import files.payload;
 //REST Assured Basic API Test
 public class Basics {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		// validate if Add Place API is working as expected
 		// given - all input details
 		// when - Submit the API there - resource, http method
 		// Then - validate the response
+		// content of the file to String -> content of file can convert into Byte -> Byte data to String
 
 		// Add place -> Update Place with New Address -> Get Place to validate if New
 		// address is present in response
 		// Set the base URI for all requests (모든 요청에서 공통으로 사용될 기본 URI 설정)
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		String response = given().log().all().queryParam("key", "qaclick123") // API key as a query parameter (API 인증 키
+		String response = given().log().all().queryParam("key", "qaclick123") // API key as a query parameter
 				.header("Content-Type", "application/json") // Tells server we're sending JSON (JSON 형식으로 보낸다고 서버에 알림)
-				.body(payload.AddPlace()) // Request body from AddPlace method (AddPlace 메서드로부터 요청 본문 받기)
+				//.body(payload.AddPlace()) // Request body from AddPlace method (AddPlace 메서드로부터 요청 본문 받기)
+				.body(Files.readAllBytes(Paths.get("src/addPlace.json"))) // Read the JSON request body from an external file and send it in the request
 				.when().post("maps/api/place/add/json") // Send POST request to the endpoint (엔드포인트에 POST 요청 보내기)
 				.then().assertThat().statusCode(200) // Verify status code is 200 (응답 코드가 200인지 확인)
 				.body("scope", equalTo("APP")) // Check if response body contains scope=APP (응답 본문에 scope 값이 APP인지 확인)
--- src/files/DynamicJson.java
+++ src/files/DynamicJson.java
@@ -1,5 +1,6 @@
 package files;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 
@@ -8,15 +9,15 @@ import io.restassured.path.json.JsonPath;
 
 public class DynamicJson {
 	
-	@Test
-	public void addBook() {
-		
+	@Test(dataProvider = "BooksData") // Bind test method with data provider
+	public void addBook(String isbn, String aisle) {
 		// Set base URI for the API
 		RestAssured.baseURI = "http://216.10.245.166";
 		
 		// Send a POST request to the Add Book API
 		String response = given().header("Content-Type", "application/json")
-		.body(payload.Addbook("sdfsadf", "35332")).when()
+		.body(payload.Addbook(isbn, aisle)) // Generate request body with parameters
+		.when() // Trigger the POST request to the API
 		.post("/Library/Addbook.php")
 		.then().log().all().assertThat().statusCode(200)
 		.extract().response().asString();
@@ -28,4 +29,16 @@ public class DynamicJson {
 		System.out.println(id);
 		
 	}
+	
+	@DataProvider(name = "BooksData") // Provide multiple sets of test data to the test method
+	public Object[][] getData() {
+		// array = collection of elements 
+		// multidimensional array = collection of arrays
+		return new Object[][] {
+			{"afsdf", "1234"}, 
+			{"aefsdv", "685464"}, 
+			{"asrgyj", "4654"}
+		};
+	}
+	
 }
