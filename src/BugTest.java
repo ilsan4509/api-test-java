cb261bad2503573b2d6f9d9d61deb3d66991c6ab
Seonggon <ilsan4509@naver.com>
2025-04-02 22:59:04 +0100

    Clean up indentation and remove unnecessary files

--- src/BugTest.java
+++ src/BugTest.java
@@ -14,23 +14,23 @@ public class BugTest {
 		
 		// Step 1: Create a Jira Issue
 		String createIssueResponse = given()
-		.header("Content-Type", "application/json") // Specify request body type
-		.header("Authorization", "Basic (secret)") // Basic Auth token for Jira API (secret)
-		.body("{\r\n"
-				+ "    \"fields\": {\r\n"
-				+ "       \"project\":\r\n"
-				+ "       {\r\n"
-				+ "          \"key\": \"SCRUM\"\r\n"
-				+ "       },\r\n"
-				+ "       \"summary\": \"menu items are not working - automation\",\r\n"
-				+ "       \"issuetype\": {\r\n"
-				+ "          \"name\": \"Bug\"\r\n"
-				+ "       }\r\n"
-				+ "   }\r\n"
-				+ "}") // Request payload containing project, summary, and issue type
-		.log().all()
-		.post("rest/api/2/issue").then().log().all().assertThat().statusCode(201) // Expect 201 Created response
-		.extract().response().asString();
+				.header("Content-Type", "application/json") // Specify request body type
+				.header("Authorization", "Basic (secret)") // Basic Auth token for Jira API (secret)
+				.body("{\r\n"
+					+ "    \"fields\": {\r\n"
+					+ "       \"project\":\r\n"
+					+ "       {\r\n"
+					+ "          \"key\": \"SCRUM\"\r\n"
+					+ "       },\r\n"
+					+ "       \"summary\": \"menu items are not working - automation\",\r\n"
+					+ "       \"issuetype\": {\r\n"
+					+ "          \"name\": \"Bug\"\r\n"
+					+ "       }\r\n"
+					+ "   }\r\n"
+					+ "}") // Request payload containing project, summary, and issue type
+				.log().all()
+				.post("rest/api/2/issue").then().log().all().assertThat().statusCode(201) // Expect 201 Created response
+				.extract().response().asString();
 		
 		// Extract issue ID from response
 		JsonPath js = new JsonPath(createIssueResponse);
@@ -38,13 +38,12 @@ public class BugTest {
 		System.out.println(issued);
 		
 		// Step 2: Upload attachment to the created issue
-		given()
-		.pathParam("key", issued) // Set dynamic issue key
-		.header("X-Atlassian-Token", "no-check") // Bypass CSRF check
-		.header("Authorization", "Basic (secret)")
-		.multiPart("file", new File("C:\\Users\\ilsan\\Downloads\\image (1).png")) // Multipart file for upload
-		.post("rest/api/2/issue/{key}/attachments").then().log().all().assertThat().statusCode(200); // Expect 200 OK after upload
-		
+		given().pathParam("key", issued) // Set dynamic issue key
+				.header("X-Atlassian-Token", "no-check") // Bypass CSRF check
+				.header("Authorization", "Basic (secret)")
+				.multiPart("file", new File("C:\\Users\\ilsan\\Downloads\\image (1).png")) // Multipart file for upload
+				.post("rest/api/2/issue/{key}/attachments").then().log().all().assertThat().statusCode(200); // Expect 200 OK after upload
+
 	}
 
 }
--- src/OAuthTest.java
+++ src/OAuthTest.java
@@ -3,33 +3,29 @@ import static io.restassured.RestAssured.given;
 import io.restassured.path.json.JsonPath;
 
 public class OAuthTest {
-	
+
 	public static void main(String[] args) {
-		
+
 		// Step 1: Get Access Token using OAuth 2.0 Client Credentials Flow
 		String response = given()
-		.formParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com") // Provide client ID
-		.formParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W") // Provide client secret
-		.formParams("grant_type", "client_credentials")
-		.formParams("scope", "trust")
-		.when().log().all()
-		.post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token").asString(); // Send POST request to get token
-		
+				.formParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com") // Provide client ID
+				.formParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W") // Provide client secret
+				.formParams("grant_type", "client_credentials").formParams("scope", "trust").when().log().all()
+				.post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token").asString(); // Send POST request to get token
+																				
 		System.out.println(response);
-		
+
 		// Step 2: Parse the JSON response to extract access token
 		JsonPath jsonPath = new JsonPath(response);
 		String accessToken = jsonPath.getString("access_token");
-		
+
 		// Step 3: Use the access token to call the protected resource API
-		String response2 = given()
-		.queryParams("access_token", accessToken)
-		.when().log().all()
-		.get("https://rahulshettyacademy.com/oauthapi/getCourseDetails") // Call protected API
-		.asString();
-		
+		String response2 = given().queryParams("access_token", accessToken).when().log().all()
+				.get("https://rahulshettyacademy.com/oauthapi/getCourseDetails") // Call protected API
+				.asString();
+
 		System.out.println(response2);
-		
+
 	}
 
 }
