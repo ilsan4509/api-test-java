a56003f9f494880a27f39dcb7f3adb7109d629df
Seonggon <ilsan4509@naver.com>
2025-04-03 22:44:27 +0100

    Add POJO classes for deserializing course API response using Jackson

---
+++ src/pojo/GetCourse.java
@@ -0,0 +1,52 @@
+package pojo;
+
+//This class maps the top-level JSON object received from the API response
+public class GetCourse {
+	
+	private String url;
+	private String services;
+	private String expertise;
+	private Courses Courses;
+	private String instructor;
+	private String linkedIn;
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getServices() {
+		return services;
+	}
+	public void setServices(String services) {
+		this.services = services;
+	}
+	public String getExpertise() {
+		return expertise;
+	}
+	public void setExpertise(String expertise) {
+		this.expertise = expertise;
+	}
+	public pojo.Courses getCourses() {
+		return Courses;
+	}
+	public void setCourses(pojo.Courses courses) {
+		Courses = courses;
+	}
+	public String getInstructor() {
+		return instructor;
+	}
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+	public String getLinkedIn() {
+		return linkedIn;
+	}
+	public void setLinkedIn(String linkedIn) {
+		this.linkedIn = linkedIn;
+	}
+
+	
+
+}
