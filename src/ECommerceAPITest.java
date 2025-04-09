d270e9d92077a78e29435083e2bf32fffceb3133
Seonggon <ilsan4509@naver.com>
2025-04-09 22:00:33 +0100

    Add product addition functionality with image upload after user login

--- src/ECommerceAPITest.java
+++ src/ECommerceAPITest.java
@@ -1,10 +1,13 @@
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
 import pojo.LoginRequest;
 import pojo.LoginResponse;
 
 import static io.restassured.RestAssured.given;
+
+import java.io.File;
 
 public class ECommerceAPITest {
 	
@@ -26,9 +29,31 @@ public class ECommerceAPITest {
 		LoginResponse loginResponse = reqLogin.when().post("/api/ecom/auth/login").then().log().all().extract().response()
 				.as(LoginResponse.class);
 		
+		// Print the token and user ID from the login response
 		System.out.println(loginResponse.getToken());
+		String token = loginResponse.getToken();
 		System.out.println(loginResponse.getUserId());
+		String userId = loginResponse.getUserId();
 		
+		// Add Product 
+		// Add Product API request setup with authorization header
+		RequestSpecification addProductBaseReq = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+				.addHeader("authorization", token)
+				.build();
+		
+		// Set up the request for adding a product, including the product details and file upload
+		RequestSpecification reqAddProduct = given().log().all().spec(addProductBaseReq).param("productName", "Laptop")
+		.param("productAddedBy", userId).param("productCategory", "fashion")
+		.param("productSubCategory", "shirts").param("productPrice", "11500")
+		.param("productDescription", "Lenova").param("productFor", "men")
+		.multiPart("productImage", new File("C:\\Users\\ilsan\\test.jpg"));
+		
+		// Send the POST request to add the product and extract the response
+		String addProductResponse = reqAddProduct.when().post("/api/ecom/product/add-product")
+		.then().log().all().extract().response().asString();
+		
+		JsonPath js = new JsonPath(addProductResponse);
+		String productId = js.get("productId");
+		System.out.println("productId= "+productId);
 	}
-	
 }
